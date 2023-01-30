29d279d1160f38f7bc9aae18c37890953c510e3f
Vitor Koetz Vaccari <123849955+VitorKoetzVaccari@users.noreply.github.com>
2023-01-30 05:45:39 -0300

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,22 @@
+
+"""
+Step 1 - Create a list and ask for the user to input some numbers.
+Step 2 - Organize them in ascending order and separete them in 2 lists (one for odd number and other for even numbers) as soon as they are added to the list
+"""
+
+lista_even=[]
+lista_odd=[]
+
+while True:
+    try:
+        x=int(input("choose a number: "))
+        if x%2==0:
+            lista_even.append(x)
+            print("even lista: ", lista_even )
+            print("odd lista: ", lista_odd)
+        else:
+            lista_odd.append(x)
+            print("even lista: ", lista_even )
+            print("odd lista: ", lista_odd)
+    except:
+        print("Invalid number")

301b3a5d093deaf3cd3406b9c6a255b42e8f3bb6
Vitor Koetz Vaccari <123849955+VitorKoetzVaccari@users.noreply.github.com>
2023-02-16 21:09:59 -0300

    Update 12_Resistors.py

--- 12_Resistors.py
+++ 12_Resistors.py
@@ -9,33 +9,42 @@ while True:
         print("U have to choose a number ... ")
         print(" ")
 
-while True:
-    try:
-        i=0 
-        while i<howmany: 
+
+i=0 
+while i<howmany: 
+    while True:
+        try:
             value=int(input("Resistor "+ str(i+1) + " value: "))
             resistor.append(value)
             print(resistor)
-            
+            break
+        except:
+            print("Value not accepted ")
+    while True:
+        try:
             if i==0:
                 R_eq=resistor[0]
                 print("R_Eq: " + str(R_eq))
-                i+=1  
+                i+=1
+                break
             else: 
                 connection=input("series (s) or parallel (p)?")
                 if connection == "s":
                     R_eq=R_eq+value
                     print("R_Eq: " + str(R_eq))
                     i+=1
+                    break
                 elif connection == "p":
-                    print("parallel")
+                    R_eq=(R_eq*value)/(R_eq+value)
+                    print("R_Eq: " + str(R_eq))
                     i+=1
+                    break
                 else:
                     print("Try again")
-                    resistor.pop(i)
                     continue
-    except:
-        print("Value not accepted ")
-       
+        except:
+            print("Value not accepted ")
+            
+    
 
-        
+  

537f1f03bd2284b23353d6138ddc8ce66b4fff23
Vitor Koetz Vaccari <123849955+VitorKoetzVaccari@users.noreply.github.com>
2023-02-23 17:43:29 -0300

    Update 13_Pyplot.py

--- 13_Pyplot.py
+++ 13_Pyplot.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 
-dominio=[]
-imagem=[]
-
+dominio=[2,3]
+imagem=[5,6]
+'''
 i=0
 while i<10:
     y=i**2
@@ -10,5 +10,11 @@ while i<10:
     imagem.append(y)
     plt.plot(dominio,imagem)
     i+=1
-    
+    if y==0:
+        print(i,y)
+    else:
+        continue
+    '''
+plt.plot(5)
+
 plt.show()

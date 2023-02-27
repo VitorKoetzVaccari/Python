6be71b8e0bb3871b14136d22e54db4f85b3e830b
Vitor Koetz Vaccari <123849955+VitorKoetzVaccari@users.noreply.github.com>
2023-02-27 19:57:40 -0300

    Update 13_Pyplot.py

--- 13_Pyplot.py
+++ 13_Pyplot.py
@@ -3,17 +3,22 @@ import matplotlib.pyplot as plt
 D=[]
 Im=[]
 O=[0,0]
-Y=[0,100]
-X=[100,0]
+Y=[0,10]
+X=[0,4]
 
-plt.plot(Y,O)
-plt.plot(O,X)
-i=-10
-while i<10:
-    y=i**2+i-6
+plt.plot(X,O)
+plt.plot(O,Y)
+i=-4
+while i<4:
+    y=i**2+i
     D.append(i)
     Im.append(y)
     plt.plot(D,Im)
-    i+=1
-
+    i+=0.1
+    if y==0:
+        print(i)
+    else:
+        continue
+print(D)
+print(Im)
 plt.show()

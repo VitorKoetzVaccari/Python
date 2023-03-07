cd7692accf75ab58f7deec523bf80f0877568b7f
Vitor Koetz Vaccari <123849955+VitorKoetzVaccari@users.noreply.github.com>
2023-03-07 18:54:53 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,8 @@
 
 print(
-"""
-Step 1 - Ask for some numbers and add them into a list. 
+"""Step 1 - Ask for some numbers and add them into a list. 
 Step 2 - Check if the input value is actually an number.
-Obs: thats an infinite list ...
-"""
+Obs: This list is only meant to have 3 values ..."""
 )
 
 lista=[]
@@ -12,10 +10,10 @@ lista=[]
 i=1
 while i<4:
         try:
-            idade=int(input("Age? "))
+            idade=int(input("\nAge? "))
             if idade != 0:
                 lista.append(idade)
-                print (lista)
+                print ("lista: ", lista)
                 i += 1
         except:
             print("Valor não aceito")

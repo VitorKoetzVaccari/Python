c4fcb6186e4040f803581eaa855cd75c86c7c272
Vitor Koetz Vaccari <123849955+VitorKoetzVaccari@users.noreply.github.com>
2023-03-09 06:22:28 -0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,14 +1,12 @@
 
-"""
-Step 1 - Create a list and ask for the user to input some numbers.
-Step 2 - Organize those numbers in ascending order as soon as they are added to the list.
-"""
+print("""Step 1 - Create a list and ask for the user to input some numbers.
+Step 2 - Organize those numbers in ascending order as soon as they are added to the list.""")
 
 lista=[]
 
 while True:
     try:
-        z=int(input("Choose a number: "))
+        z=int(input("\nChoose a number: "))
         lista.append(z) 
         a=0
         while a<len(lista):
@@ -17,12 +15,11 @@ while True:
                 if lista[b]<lista[a] and b>a:
                     lista.insert(a,lista[b])
                     lista.pop(b+1)
-                    print(lista)
                     b+=1
                 else:
-                    print(lista)
                     b+=1
             a+=1
+        print(lista)
     except:
         print("wrong value")
 
